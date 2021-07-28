8ce831de672f74a30ff2686139e912f1fd52fb96
Nickollas Aranha <nickollas.aranha@gmail.com>
2021-07-28 15:49:45 -0300

    fix: ensure EmailValidation allows empty value

--- src/validation/validators/email/email-validation.spec.ts
+++ src/validation/validators/email/email-validation.spec.ts
@@ -7,13 +7,19 @@ const makeSut = (): EmailValidation => new EmailValidation(faker.database.column
 describe('EmailValidation', () => {
   test('Should return error if email is invalid', () => {
     const sut = makeSut()
-    const error = sut.validate('')
+    const error = sut.validate(faker.random.word())
     expect(error).toEqual(new InvalidFieldError())
   })
 
   test('Should return false if email is valid', () => {
     const sut = makeSut()
     const error = sut.validate(faker.internet.email())
+    expect(error).toBeFalsy()
+  })
+
+  test('Should return false if email is empty', () => {
+    const sut = makeSut()
+    const error = sut.validate('')
     expect(error).toBeFalsy()
   })
 })
--- src/validation/validators/email/email-validation.ts
+++ src/validation/validators/email/email-validation.ts
@@ -6,6 +6,6 @@ export class EmailValidation implements FieldValidation {
 
   validate (value: string): Error {
     const emnailRegex = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/gi//
-    return emnailRegex.test(value) ? null : new InvalidFieldError()
+    return (!value || emnailRegex.test(value)) ? null : new InvalidFieldError()
   }
 }
