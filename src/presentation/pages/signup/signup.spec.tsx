5b88590539a0e349bf0249012785d256284912b6
Nickollas Aranha <nickollas.aranha@gmail.com>
2021-07-31 16:04:58 -0300

    test: ensure SignUp shows valid state for fields

--- src/presentation/pages/signup/signup.spec.tsx
+++ src/presentation/pages/signup/signup.spec.tsx
@@ -78,4 +78,16 @@ describe('Login component', () => {
     Helper.populateField(sut, 'email')
     Helper.testStatusForField(sut, 'email')
   })
+
+  test('Should show valid password state if Validation succeeds', () => {
+    const { sut } = makeSut()
+    Helper.populateField(sut, 'password')
+    Helper.testStatusForField(sut, 'password')
+  })
+
+  test('Should show valid passwordConfirmation state if Validation succeeds', () => {
+    const { sut } = makeSut()
+    Helper.populateField(sut, 'passwordConfirmation')
+    Helper.testStatusForField(sut, 'passwordConfirmation')
+  })
 })
